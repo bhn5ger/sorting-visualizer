b575da65f2c58dc59cb12d261b6e034b7caa7964
bhn5ger <69608929+bri2high@users.noreply.github.com>
2020-12-27 17:17:05 -0500

    Added colors to merge sort

--- src/com/sorting/visualizer/MergeSort.java
+++ src/com/sorting/visualizer/MergeSort.java
@@ -13,6 +13,25 @@ public class MergeSort {
     // Second subarray is arr[m+1..r]
     static void merge(JPanel arr[], int l, int m, int r)
     {
+		SinusoidSegment.segments[m].setColor(Color.blue); 
+		SinusoidSegment.segments[m].repaint();
+    	
+		SinusoidSegment.segments[l].setColor(darkGreen); 
+		SinusoidSegment.segments[l].repaint();
+		
+		SinusoidSegment.segments[r].setColor(darkGreen); 
+		SinusoidSegment.segments[r].repaint();
+		
+
+		
+		try {
+			Sort.displayLoadingCaption(0, "merge sort"); 
+			Thread.sleep(speed + 200); 
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		
         // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
@@ -21,11 +40,20 @@ public class MergeSort {
         JPanel L[] = new JPanel[n1];
         JPanel R[] = new JPanel[n2];
  
+        SinusoidSegment L2[] = new SinusoidSegment[n1];
+        SinusoidSegment R2[] = new SinusoidSegment[n2];
+        
         /*Copy data to temp arrays*/
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
+        for (int i = 0; i < n1; ++i) {
+        	L[i] = arr[l + i];
+        	
+        	L2[i] = SinusoidSegment.segments[l + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+        	R[j] = arr[m + 1 + j];
+        	
+        	R2[j] = SinusoidSegment.segments[m + 1 + j];
+        }
  
         /* Merge the temp arrays */
  
@@ -36,24 +64,31 @@ public class MergeSort {
         int k = l;
         int totalLength = (int)arr[k].getBounds().getX();
         while (i < n1 && j < n2) {
+
             if (L[i].getBounds().getWidth() <= R[j].getBounds().getWidth()) {
                 arr[k] = L[i];
+                SinusoidSegment.segments[k] = L2[i];
             	Window.segmentPanels[k].setBounds(totalLength, Window.cycleYPos, (int)arr[k].getBounds().getWidth(),100);
             	totalLength += (int)arr[k].getBounds().getWidth();
                 i++;
+
             }
             else {
                 arr[k] = R[j];
+                SinusoidSegment.segments[k] = R2[j];
             	Window.segmentPanels[k].setBounds(totalLength, Window.cycleYPos, (int)arr[k].getBounds().getWidth(),100);
             	totalLength += (int)arr[k].getBounds().getWidth();
                 j++;
+                
             }
+
             k++;
         }
  
         /* Copy remaining elements of L[] if any */
         while (i < n1) {
             arr[k] = L[i];
+            SinusoidSegment.segments[k] = L2[i];
         	Window.segmentPanels[k].setBounds(totalLength, Window.cycleYPos, (int)arr[k].getBounds().getWidth(),100);
         	totalLength += (int)arr[k].getBounds().getWidth();
             i++;
@@ -63,29 +98,51 @@ public class MergeSort {
         /* Copy remaining elements of R[] if any */
         while (j < n2) {
             arr[k] = R[j];
+            SinusoidSegment.segments[k] = R2[j];
         	Window.segmentPanels[k].setBounds(totalLength, Window.cycleYPos, (int)arr[k].getBounds().getWidth(),100);
         	totalLength += (int)arr[k].getBounds().getWidth();
             j++;
             k++;
 
         }
+        
+        
+		
+		SinusoidSegment.segments[l].setColor(Color.black); 
+		SinusoidSegment.segments[l].repaint();
+		
+		SinusoidSegment.segments[r].setColor(Color.black); 
+		SinusoidSegment.segments[r].repaint();
+		
+		SinusoidSegment.segments[m].setColor(Color.black); 
+		SinusoidSegment.segments[m].repaint();
+		
+		//Sort.clearColors(r);
+		try {
+			Sort.displayLoadingCaption(1, "merge sort"); //delay
+			Thread.sleep(speed + 200); 
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
     }
  
     // Main function that sorts arr[l..r] using
     // merge()
     public static void sort(JPanel arr[], int l, int r)
     {
-    	int p = 0;
+
         	if (l < r) {
         		// Find the middle point
         		int m = (l + r) / 2;
- 
+        	
         		// Sort first and second halves
         		sort(arr, l, m);
         		sort(arr, m + 1, r);
- 
+        		
         		// Merge the sorted halves
         		merge(arr, l, m, r);
+        		
+        		Sort.clearColors(r);
         	}
         	
         	
@@ -100,3 +157,26 @@ public class MergeSort {
     
 	
 }
+
+//set l/r green
+//set m blue
+
+//set inner cycles red
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
